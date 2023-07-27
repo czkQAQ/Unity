063452cb776b5bd06ec92d6f0407143293df4815
czkQAQ <140473956+czkQAQ@users.noreply.github.com>
2023-07-27 20:12:44 +0800

    背景图片管理
    通过读取配置表来对游戏背景进行设置（我第一次学习json文件的读取）。

---
+++ 5.BGManager/BGItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGItem : MonoBehaviour
+{
+    public int Id;
+    public string KeyName;
+    public string background;
+    public string map;
+}
---
+++ 5.BGManager/BGManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using UnityEngine.SceneManagement;
+
+public class BGManager : MonoBehaviour
+{
+    private static BGManager instance;
+    public static BGManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<BGManager>();
+                if(instance == null)
+                {
+                    GameObject obj = new GameObject("BGManager");
+                    instance = obj.AddComponent<BGManager>();
+                    DontDestroyOnLoad(obj);
+                }
+            }
+            return instance;
+        }
+    }
+
+    Dictionary<string, string> BGDic = new Dictionary<string, string>();
+    Dictionary<string, Sprite> SpriteDic = new Dictionary<string, Sprite>();
+    private GameObject Level;
+    private GameObject Background;
+
+
+    public void Init()
+    {
+        
+        LoadConfig();
+        AddSprite();
+    }
+
+    void LoadConfig()
+    {
+        var json = Resources.Load<TextAsset>("level");
+        var config = JsonMapper.ToObject<List<BGItem>>(json.text);
+        foreach (var item in config)
+        {
+            BGDic.Add(item.KeyName + "(Clone)",item.background);
+        }
+    }
+
+    void AddSprite()
+    {
+        //SpriteDic.Add("bg0", Resources.Load<Sprite>("Images/InGame/Game_Background/bg0"));
+        //SpriteDic.Add("bg1", Resources.Load<Sprite>("Images/InGame/Game_Background/bg1"));
+        //SpriteDic.Add("bg2", Resources.Load<Sprite>("Images/InGame/Game_Background/bg2"));
+        //SpriteDic.Add("bg3", Resources.Load<Sprite>("Images/InGame/Game_Background/bg3"));
+
+        for (int i = 0;i < 100;i ++)
+        {
+            SpriteDic.Add("bg" + i.ToString(), Resources.Load<Sprite>("Images/InGame/Game_Background/bg" + i));
+        }
+    }
+
+
+    /*private void Update()
+    {
+        if (Application.loadedLevelName.Equals("MainGame"))
+        {
+            Level = GameObject.FindObjectOfType<MapLevelManager>().gameObject;
+            
+
+            string bgType = BGDic[Level.name];
+            Background.gameObject.GetComponent<Image>().sprite = SpriteDic[bgType];
+        }
+    }*/
+
+    public void SetBg(string name)
+    {
+        string bgType = BGDic[name];
+        Background = GameObject.Find("Panel-Background");
+        Background.gameObject.GetComponent<Image>().sprite = SpriteDic[bgType];
+    }
+}
